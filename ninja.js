3d0271544ed1d95aa9e2bd9f0eadbbd431a8f9e1
Ddavila09 <ddavilax23@gmail.com>
2023-05-02 13:55:03 -0500

    Ninja

---
+++ ninja.js
@@ -0,0 +1,25 @@
+class Ninja{
+    constructor(name, health = 10, speed = 3, strength = 3){
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+    sayName(){
+        console.log(`My name is ${this.name} and I will be #1!`)
+    }
+    showStats(){
+        console.log(`Name: ${this.name}, Health: ${this.health}, Speed: ${this.speed}, Strength: ${this.strength}`);
+    }
+    drinkSake(){
+        this.health += 10;
+        console.log(`Time for some sake!`)
+    }
+}
+
+const ninja = new Ninja("David")
+
+console.log(ninja)
+ninja.sayName()
+ninja.drinkSake()
+ninja.showStats()
